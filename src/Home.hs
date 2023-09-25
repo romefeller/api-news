5f772fd6596db1047f2b37f137c01fdf5a7a2e15
Alexandre Garcia de Oliveira <romefeller@MacBook-Pro-de-Alexandre.local>
2023-09-25 19:36:11 -0300

    New project created with News module

---
+++ src/Home.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+module Home where
+
+import Foundation
+import Yesod.Core
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout $ do
+    setTitle "Minimal Multifile"
---
+++ src/News.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+module News where
+
+import Foundation
+import Yesod.Core
+
+data News = News
