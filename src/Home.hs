073416a4a0c8a93cae9146b9c23b1b0dfa0f53b4
Alexandre Garcia de Oliveira <romefeller@MacBook-Pro-de-Alexandre.local>
2023-09-25 20:22:47 -0300

    Getting all news

--- src/Home.hs
+++ src/Home.hs
@@ -3,13 +3,37 @@
 module Home where
 
 import Foundation
-import Data.Text.Lazy.Encoding as LE
+
+import Control.Lens hiding (children, element, (.=))
+import Control.Monad
+import qualified Data.Text.Lazy.Encoding as LE
+import qualified Data.Text.Lazy as LT
+import Data.Aeson (object, (.=))
+
+import Network.Wreq
+import Data.Text
+
+import Text.Taggy.Renderer
 import Text.Taggy
 import Text.Taggy.Lens
 
 import Yesod.Core
 
+allNews :: IO [Text]
+allNews = do
+    let url = "https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=fb456ddbe7f03b12d69ee93d7f2d4191"
+    respBody <- get url
+    return $ respBody ^.. responseBody . to LE.decodeUtf8 . to LT.toStrict
 
-getHomeR :: Handler Html
-getHomeR = defaultLayout $ do
-    setTitle "Minimal Multifile"
+
+getHomeR :: Handler Value
+getHomeR = do
+    news <- liftIO allNews
+    forM_ news $ \item -> liftIO $ putStrLn (unpack item)
+
+    returnJson $ object ["data" .= news]
+
+
+
+
+
